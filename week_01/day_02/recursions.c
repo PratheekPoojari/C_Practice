c39e937f9036bc32fa5bf7ab4180fc53de3b9957
PratheekPoojari <pratheekspoojari1304@gmail.com>
2026-07-01 07:24:18 +0530

    Modified recursions.c to show execution time and added iterative fibonacci

--- week_01/day_02/recursions.c
+++ week_01/day_02/recursions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 long long itr_factorial(int n)
 {
@@ -10,17 +11,16 @@ long long itr_factorial(int n)
     return fact;
 }
 
-unsigned long long rec_factorial(int n)
+long long rec_factorial(int n)
 {
     if(n == 0)
     {
         return 1;
     }
-
-    return n * rec_factorial(n - 1); // recursive case - calls itself with a smaller input 
+    return n * rec_factorial(n - 1);
 }
 
-int fibonacci(int n)
+long long rec_fibonacci(int n)
 {
     if(n == 0)
     {
@@ -32,27 +32,58 @@ int fibonacci(int n)
     }
     else
     {
-        return (fibonacci(n - 1) + fibonacci(n - 2)); // didnt really understand this, so just googled it.
+        return (rec_fibonacci(n - 1) + rec_fibonacci(n - 2));
     }
+}
+
+long long itr_fibonacci(int n)
+{
+    long long first = 0, second = 1, fibo;
+
+    for(int i=0; i<n; i++)
+    {
+        fibo = first;
+        first = first + second;
+        second = fibo;
+    }
+    return first;
 }
 
 int main()
 {
     int a, b;
+    clock_t start, end;
+    double time_taken;
+
     printf("Enter the number to get its factorial:\n");
     scanf("%d", &a);
 
+    start = clock();
     long long i_factorial = itr_factorial(a);
-    printf("The factorial of %d through iteration is: %lld\n", a, i_factorial);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The factorial of %d through iteration is: %lld (took %lf sec)\n", a, i_factorial, time_taken);
 
-    unsigned long long r_factorial = rec_factorial(a);
-    printf("The factorial of %d through recursion is: %lld\n", a, r_factorial);
+    start = clock();
+    long long r_factorial = rec_factorial(a);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The factorial of %d through recursion is: %lld (took %lf sec)\n", a, r_factorial, time_taken);
 
     printf("Enter the number to get its fibonacci sequence:\n");
     scanf("%d", &b);
 
-    int r_fibonacci = fibonacci(b);
-    printf("The fibonacci sequence value of %d is: %d\n", b, r_fibonacci);
+    start = clock();
+    long long r_fibonacci = rec_fibonacci(b);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Fibonacci (recursive) of %d is: %lld (took %lf sec)\n", b, r_fibonacci, time_taken);
+
+    start = clock();
+    long long i_fibonacci = itr_fibonacci(b);
+    end = clock();
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Fibonacci (iterative) of %d is: %lld (took %lf sec)\n", b, i_fibonacci, time_taken);
 
     return 0;
 }
